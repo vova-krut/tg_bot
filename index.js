599296c5170fde0f3013c9726761ed2c9e9b8ac1
Volodymyr <vthehang@gmail.com>
2022-05-13 18:20:43 +0300

    added /info command & command menu

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const token = "5379480787:AAFozwCRTLz_wF6isnsEMA4CsPRMGJ74Uww";
 
 const bot = new TelegramApi(token, { polling: true });
 
+bot.setMyCommands([
+    { command: "/start", description: "Starting hello" },
+    { command: "/info", description: "Get user info" },
+]);
+
 bot.on("message", async (msg) => {
     const text = msg.text;
     const chatId = msg.chat.id;
@@ -13,6 +18,13 @@ bot.on("message", async (msg) => {
         await bot.sendSticker(
             chatId,
             "https://tlgrm.ru/_/stickers/504/bae/504bae9c-f76b-33b6-aac7-2c8dbc312a78/48.webp"
+        );
+    }
+
+    if (text === "/info") {
+        await bot.sendMessage(
+            chatId,
+            `Your name is ${msg.from.first_name} ${msg.from.last_name}`
         );
     }
 });
